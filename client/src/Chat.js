7ca44992ff58bc654a4f959ee0946cf9dfd03756
Alex Alikhani <alikhanialex@gmail.com>
2022-07-02 20:13:04 +0100

    User can now also see the message that they sent

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -17,6 +17,7 @@ const Chat = ({ socket, username, room }) => {
       };
 
       await socket.emit("send_message", messageData);
+      setMessageList((list) => [...list, messageData]);
     }
   };
 
