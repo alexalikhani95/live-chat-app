2d5eb4feebc1e829e47dacdbab277358b5f94e11
Alex Alikhani <alikhanialex@gmail.com>
2022-07-02 17:01:53 +0100

    Setting up socket.io

--- server/index.js
+++ server/index.js
@@ -2,11 +2,24 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
+const { Server } = require("socket.io");
 
 app.use(cors());
 
 // generate server
 const server = http.createServer(app);
+
+const io = new Server(server, {
+  // passing a cors object to help deal with issues in socket.io server
+  cors: {
+    origin: "http://localhost:3000", // Accept this url as this is where the react app will be running
+    methods: ["GET", "POST"], // These methods will be accepted
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+}); // io.on will listen for the "connection" event
 
 server.listen(3001, () => {
   console.log("Server Running!");
