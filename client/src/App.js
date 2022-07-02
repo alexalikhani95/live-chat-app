beaad9cdcb1dea45ceb4a0986ef489e929db883f
Alex Alikhani <alikhanialex@gmail.com>
2022-07-02 17:08:36 +0100

    Connecting socket.io on the frontend to the backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:3001"); // connect to the socket.io server on the backend
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <p>Hello</p>
+    </div>
+  );
 }
 
 export default App;
